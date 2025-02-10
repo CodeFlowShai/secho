4b28f1abbb8b92537606dcf7a6e476ca0d55c4d6
CodeFlowShai <verblemathew4@gmail.com>
2025-02-10 09:12:41 -0600

    V0.2
    Added -r flag to reverse output

--- main.c
+++ main.c
@@ -7,14 +7,16 @@ void out(char str[]) {
 void err(char str[]) {
     write(2, str, strlen(str));
 }
+
 int main(int argc, char *argv[]) 
 {
     int times = 1;
     char *s = NULL;
     int s_provided = 0;
     int outNl = 1;
+    int reverse = 0;
     int opt;
-    while ((opt = getopt(argc, argv, "n:s:lh")) != -1) {
+    while ((opt = getopt(argc, argv, "n:s:rlh")) != -1) {
         switch (opt) {
             case 'n':
                 times = atoi(optarg);
@@ -29,9 +31,12 @@ int main(int argc, char *argv[])
             case 'l':
                 outNl = 0;
                 break;
+            case 'r':
+                reverse = 1;
+                break;
             default:
-                out("Usage: secho -s <string> [-n <number>] [-l] [-h]\nOptions:\n  -s <string>   Specify the string to print (required).\n  -n <number>   Repeat the string the specified number of times.\n  -l            Print all repetitions on the same line without newlines.\n  -h            Show this help message and exit.\n");
-                return 1;
+                err("Invalid input!\nUsage: secho -s <string> [-n <number>] [-l] [-h]\nOptions:\n  -s <string>   Specify the string to print (required).\n  -n <number>   Repeat the string the specified number of times.\n  -l            Print all repetitions on the same line without newlines.\n  -h            Show this help message and exit.\n");
+                exit(1);
         }
     }
     if (times <= 0) {
@@ -41,8 +46,21 @@ int main(int argc, char *argv[])
         err("Error: -s <string> is required!\n");
         return 1;
     }
+    char rev_s[strlen(s) + 1];
+    if (reverse == 1) {
+        for (int i = 0; i < strlen(s); i++) { 
+            rev_s[i] = s[strlen(s) - 1 - i];
+        }
+        rev_s[strlen(s)] = 0;
+    }
+
     for (int i = 0; i < times; i++) {
-        out(s);
+        if (reverse = 1) {
+            out(rev_s);
+        }
+        else {
+            out(s);
+        }
         if (outNl == 1) {
             out("\n");
         }
